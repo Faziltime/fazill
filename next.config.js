66fd17cdd222a3d287e881267c18e6731d77898f
Fazil <fazil.timedestiny@gmail.com>
2025-09-24 21:43:36 +0530

    Update next.config.js with proper configuration - force new deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin']
+  },
+  images: {
+    domains: ['ui-avatars.com']
+  }
 };
 
 module.exports = nextConfig;
